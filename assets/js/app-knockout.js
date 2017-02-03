804e5aa765428f5e160d58b38842a106c3889928
Eli Anderson <eli@theprogrampdx.com>
2017-02-03 15:46:29 -0800

    Get the files back in order and get the React "HTML" done

--- assets/js/app-knockout.js
+++ assets/js/app-knockout.js
@@ -1,1 +1,53 @@
-var Condiment = function(name, price) {  this.name = name;  this.price = price;  this.onBurger = ko.observable(false);}var BurgerBuilder = function(){  var self = this  self.selectedCheese = ko.observable();  self.basePrice = 5  self.showModal = ko.observable(false);  self.test = ko.observable('hi');  self.showTest = function(){    console.log(test)  }  self.cheeses = ko.observableArray([    new Condiment('Cheddar', 2),    new Condiment('Swiss', 1),    new Condiment('Provolone', 3),    new Condiment('American', 1),    new Condiment('Colby Jack', 2),  ])  self.sauces = ko.observableArray([    new Condiment('Ketchup', 1),    new Condiment('Mustard', 5),    new Condiment('Mayo', 1),    new Condiment('BBQ', 2),  ])  self.veggies = ko.observableArray([    new Condiment('Tomato', 1),    new Condiment('Onion', 5),    new Condiment('Lettuce', 1),    new Condiment('Pickles', 1),    new Condiment('Bacon (yes its a veggie)', 2),  ])  self.toggleModal = function() { self.showModal(!self.showModal()); }  self.total = ko.pureComputed(function(){    price = self.basePrice    $.each(self.veggies().concat(self.sauces()), function(index, topping) {      if(topping.onBurger()){        price += topping.price      }    })    if (self.selectedCheese() != undefined) {      price += self.selectedCheese()    }    return price  });};ko.applyBindings(new BurgerBuilder());
+var Condiment = function(name, price) { 
+    this.name = name; 
+    this.price = price; 
+    this.onBurger = ko.observable(false);
+}
+
+var BurgerBuilder = function() { 
+  var self = this;
+  self.selectedCheese = ko.observable(); 
+  self.basePrice = 5;
+  self.showModal = ko.observable(false); 
+
+  self.cheeses = ko.observableArray([
+    new Condiment('Cheddar', 2),
+    new Condiment('Swiss', 1),
+    new Condiment('Provolone', 3),
+    new Condiment('American', 1),
+    new Condiment('Colby Jack', 2),  ]);
+
+  self.sauces = ko.observableArray([
+    new Condiment('Ketchup', 1),
+    new Condiment('Mustard', 5),
+    new Condiment('Mayo', 1),
+    new Condiment('BBQ', 2),  ]);
+
+  self.veggies = ko.observableArray([
+    new Condiment('Tomato', 1),
+    new Condiment('Onion', 5),
+    new Condiment('Lettuce', 1),
+    new Condiment('Pickles', 1),
+    new Condiment('Bacon (yes its a veggie)', 2),  ]);
+
+  self.toggleModal = function() {
+    self.showModal(!self.showModal());
+  } 
+
+  self.total = ko.pureComputed(function() {   
+      price = self.basePrice;
+
+      $.each(self.veggies().concat(self.sauces()), function(index, topping) {     
+        if (topping.onBurger()) {       
+          price += topping.price     
+        }   
+      });
+
+      if (self.selectedCheese() != undefined) {     
+        price += self.selectedCheese()   
+      }   
+      return price 
+  });
+};
+
+ko.applyBindings(new BurgerBuilder());
--- assets/js/app-react.js
+++ assets/js/app-react.js
@@ -1,1 +1,74 @@
-var rootElement =  React.createElement('div', {},    React.createElement('div', {className: 'row'},      React.createElement('div', {className: 'large-12 columns'},        React.createElement('h1', {}, 'Custom Burger Builder')      )    ),    React.createElement('div', {className: 'row'},      React.createElement('div', {className: 'large-12 columns'},        React.createElement('div', {className: 'callout'},          React.createElement('form', {},            React.createElement('div', {className: 'row'},              React.createElement('div', {className: 'large-9 columns'},                React.createElement('label', {}, 'Name')                // React.createElement('input', {}, 'Name')    //    // )))    )))))))ReactDOM.render(rootElement, document.getElementById('react-app'))
+var burgerBuilder =
+  React.createElement('div', {},
+    React.createElement('div', {className: 'row'},
+      React.createElement('div', {className: 'large-12 columns'},
+        React.createElement('h1', {className: 'large-12 columns'}, 'Custom Burger Builder')
+      ),
+      React.createElement('div', {},
+        React.createElement('div', {className: 'row'},
+          React.createElement('div', {className: 'large-12 columns'},
+            React.createElement('div', {className: 'callout'},
+              React.createElement('form', {},
+                React.createElement('div', {className: 'row'},
+
+                  React.createElement('div', {className: 'large-9 columns'},
+                    React.createElement('label', {},'Name'),
+                    React.createElement('input', {id: 'name', type: 'text', placeholder: 'Epic Burger Name'})
+                  ),
+
+                  React.createElement('div', {className: 'large-3 medium-3 columns'},
+                    React.createElement('div', {className: 'row collapse'},
+                      React.createElement('label', {}, 'Burger Price')
+                    ),
+                    React.createElement('div', {className: 'input-group'},
+                      React.createElement('span', {className: 'input-group-label'}, '$'),
+                      React.createElement('input', {id: 'price', className: 'input-group-field'})
+                    )
+                  )
+                ),
+
+                React.createElement('div', {className: 'row'},
+
+                  React.createElement('div', {className: 'large-4 columns'},
+                      React.createElement('label', {}, 'Cheese'),
+                      React.createElement('select', {})
+                  ),
+                  React.createElement('div', {className: 'large-8 medium-8 columns'},
+                      React.createElement('label', {}, 'Sauces'),
+                      React.createElement('div', {},
+                        React.createElement('input', {type: 'checkbox'}),
+                        React.createElement('label', {}, 'Sauce')
+                      )
+                   )
+                 ),
+                React.createElement('div', {className: 'row'},
+                  React.createElement('div', {className: 'large-12 columns'},
+                    React.createElement('p', {className: 'button'}, 'Add Veggies')
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    ),
+    React.createElement('div', {className: 'modal-wrapper'},
+      React.createElement('div', {className: 'modal-container'},
+        React.createElement('div', {className: 'modal'},
+          React.createElement('a', {className: 'close-modal'},'x'),
+          React.createElement('div', {id: 'modal-content'},
+            React.createElement('div', {className: 'veggie-options'},
+              React.createElement('label', {}, 'Veggies'),
+              React.createElement('div', {},
+                React.createElement('input', {type: 'checkbox'}),
+                React.createElement('label', {}, 'Veggie')
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+
+ReactDOM.render(burgerBuilder, document.getElementById('react-app'))
+
